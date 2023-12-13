62d2348865dd42e3fdeba37984d4c5f7a7031b9c
Janaye Cheong <janaye.c13@gmail.com>
2023-12-12 16:56:39 -0800

    feat: implemented partial user.go methods

--- store/user.go
+++ store/user.go
@@ -3,7 +3,8 @@ package store
 import (
 	"context"
 	"time"
-
+	"strconv"
+	"fmt"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -30,29 +31,59 @@ type pgUserStore struct {
 }
 
 func NewPGUserStore(db *sqlx.DB) UserStore {
-	return &pgUserStore{db}
+	return &pgUserStore{db} 
 }
 
 func (s *pgUserStore) GetAll(ctx context.Context) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u, "SELECT * FROM users")
 	if err != nil {
-		return nil, err
+		return nil,fmt.Errorf("Unable to retrieve users from database")
 	}
 	return u, nil
 }
 
 // implement all other methods
 func (s *pgUserStore) GetOne(ctx context.Context, id int) (User, error) {
-	return User{}, nil
+	var u User 
+	err := s.db.SelectContext(ctx, &u, "SELECT * FROM users WHERE id = $1", id)
+	if err != nil {
+		
+		return User{}, fmt.Errorf("Unable to find user with id")
+	}
+	return u, nil 
 }
 
 func (s *pgUserStore) GetOneByStudentID(ctx context.Context, studentID string) (User, error) {
-	return User{}, nil
+    var u User
+
+    // Check if the length of the studentID is 8 characters
+    if len(studentID) != 8 {
+        return User{}, fmt.Errorf("studentID must be 8 digits long, got %d", len(studentID))
+    }
+
+    // Convert string parameter to studentID integer type 
+    value, err := strconv.Atoi(studentID)
+    if err != nil {
+        return User{}, fmt.Errorf("studentID must be a valid integer")
+    }
+
+    err = s.db.GetContext(ctx, &u, "SELECT * FROM users WHERE student_id = $1", value)
+    if err != nil {
+        return User{}, fmt.Errorf("Unable to find user with studentID %d", value)
+    }
+    return u, nil 
 }
 
+
 func (s *pgUserStore) GetOneByEmail(ctx context.Context, email string) (User, error) {
-	return User{}, nil
+	var u User 
+	
+	err := s.db.GetContext(ctx, &u, "SELECT * FROM users WHERE TRIM(LOWER(email)) = TRIM(LOWER($1))", email)
+	if err != nil {
+		return User{}, fmt.Errorf("Unable to find user with email %s", email)
+	}
+	return u, nil
 }
 
 func (s *pgUserStore) Create(ctx context.Context, user User) (User, error) {
