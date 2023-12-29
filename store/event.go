658ae49ecf93c49e7989412143925240376fe8fe
Karen Zhao <karenzhao35@gmail.com>
2023-12-29 23:04:58 +0000

    fix: fixed issues within previous commit

--- store/event.go
+++ store/event.go
@@ -2,7 +2,6 @@ package store
 import (
     "context"
     "fmt"
-    "strconv"
     "time"
     "github.com/jmoiron/sqlx"
 )
@@ -20,7 +19,7 @@ type Event struct {
 type EventStore interface {
     GetAll(ctx context.Context) ([]Event, error)
     GetOne(ctx context.Context, id int) (*Event, error)
-    GetOneByStartDate(ctx context.Context, startDate string) (*Event, error)
+    GetOneByStartDate(ctx context.Context, startDate time.Time) (*Event, error)
     GetOneByName(ctx context.Context, name string) (*Event, error) // ??
     Create(ctx context.Context, event Event) (*Event, error)
     Update(ctx context.Context, event Event) (*Event, error)
@@ -35,49 +34,57 @@ func NewPGEventStore(db *sqlx.DB) EventStore {
     return &pgEventStore{db}
 }
 
-func (s *pgUserStore) GetAll(ctx context.Context) ([]Event, error) {
+func (s *pgEventStore) GetAll(ctx context.Context) ([]Event, error) {
     var e []Event
     err := s.db.SelectContext(ctx, &e, "SELECT * FROM events")
     if err != nil {
-        return nil, fmt.Errorf("Unable to retrieve events from database, error %w", err)
+        return nil, fmt.Errorf("unable to retrieve events from database, error %w", err)
     }
     return e, nil
 }
 
-func (s *pgUserStore) GetOne(ctx context.Context, id int) (*Event, error) {
+func (s *pgEventStore) GetOne(ctx context.Context, id int) (*Event, error) {
     var e Event
     err := s.db.GetContext(ctx, &e, "SELECT * FROM events WHERE id = $1", id)
     if err != nil {
-        return nil, fmt.Errorf("Unable to find event with id, error %w", err)
+        return nil, fmt.Errorf("unable to find event with id, error %w", err)
     }
-    return &u, nil
+    return &e, nil
 }
 
 
-func (s *pgUserStore) GetOneByName(ctx context.Context, name string) (*Event, error) {
+func (s *pgEventStore) GetOneByName(ctx context.Context, name string) (*Event, error) {
     var e Event
     err := s.db.GetContext(ctx, &e, "SELECT * FROM events WHERE LOWER(name) = LOWER($1)", name)
     if err != nil {
-        return nil, fmt.Errorf("Unable to find event with name %s, with error %w", name, err)
+        return nil, fmt.Errorf("unable to find event with name %s, with error %w", name, err)
     }
     return &e, nil
 }
 
 // Don't know if I handled date right here
-func (s *pgUserStore) GetOneByStartDate(ctx context.Context, date time.Time) (*Event, error) {
+func (s *pgEventStore) GetOneByStartDate(ctx context.Context, date time.Time) (*Event, error) {
     var e Event
     err := s.db.GetContext(ctx, &e, "SELECT * FROM events WHERE start_date = $1", date)
     if err != nil {
-        return nil, fmt.Errorf("Unable to find event with start date %s, with error %w", date, err)
+        return nil, fmt.Errorf("unable to find event with start date %s, with error %w", date, err)
     }
     return &e, nil
 }
 
-func (s *pgUserStore) Create(ctx context.Context, event Event) (*Event, error) {
+func (s *pgEventStore) Create(ctx context.Context, event Event) (*Event, error) {
     query := "INSERT INTO events (name, description, start_date, end_date, volunteers_required) VALUES ($1, $2, $3, $4, $5) RETURNING id, created_at"
-    err = s.db.QueryRowContext(ctx, query, event.Name, event.Description, event.StartDate, event.EndDate, event.VolunteersRequired).Scan(&event.ID, &event.CreatedAt)
+    err := s.db.QueryRowContext(ctx, query, event.Name, event.Description, event.StartDate, event.EndDate, event.VolunteersRequired).Scan(&event.ID, &event.CreatedAt)
     if err != nil {
-        return nil, fmt.Errorf("Unable to create and store an event, error %w", err)
+        return nil, fmt.Errorf("unable to create and store an event, error %w", err)
     }
     return &event, nil 
+}
+
+
+func (s *pgEventStore) Update(ctx context.Context, event Event) (*Event, error) {
+    return nil, nil
+}
+func (s *pgEventStore) Delete(ctx context.Context, id int) error {
+    return nil
 }
