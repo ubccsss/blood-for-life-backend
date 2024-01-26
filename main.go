82790ac3ad460e7a5086e145b5b295ee57e6833f
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-01-26 13:03:12 -0800

    implemented post request

--- main.go
+++ main.go
@@ -1,15 +1,50 @@
 package main
 
 import (
+	"blood-for-life-backend/store"
 	"net/http"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
+
+	db, err := sqlx.Connect() // configuration needed
+
+	if err != nil {
+		panic(err)
+	}
+
+	eventStore := store.NewPGEventStore(db)
+	bind(e, eventStore)
+
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
+
+func bind(e *echo.Echo, eventStore store.EventStore) {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.POST("/create", func(c echo.Context) error {
+		event := new(store.Event)
+
+		// parse request body
+		bindErr := c.Bind(event)
+
+		if bindErr != nil {
+			return c.JSON(http.StatusBadRequest, bindErr)
+		}
+
+		_, err := eventStore.Create(c.Request().Context(), *event)
+
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+
+		return c.JSON(http.StatusOK, event)
+	})
 }
